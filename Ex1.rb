a1e50e77fdf3eceae4484e94521984a49705b6ab
Victoria <mvictoria.cavo@gmail.com>
2015-11-22 23:03:33 -0300

    ex1 correcciones

---
+++ Ex1.rb
@@ -0,0 +1,26 @@
+require 'bio'
+
+if ARGV.length != 1
+  raise 'Invalid amount of arguments!'
+end
+
+file_name = ARGV[0].to_s
+file_len = file_name.length
+
+if file_len < 4 || !file_name[file_len-3,3]==".gb" || !(file_len>4 && file_name[file_len-4,4]==".gbk") 
+  raise "Invalid file extension! Must be .gb or .gbk"
+end
+
+genbank_content = Bio::GenBank.open(file_name)
+
+string_sequence = ''
+
+genbank_content.each_entry do |gc|
+  string_sequence << gc.to_biosequence
+end
+
+6.times do |frame|
+  File.open("fastaframe#{frame + 1}.fa", 'w') do |f|
+    f.write(Bio::Sequence::NA.new(string_sequence).translate(frame + 1,1,'_').to_fasta)
+  end
+end
