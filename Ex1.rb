c71af5d0984d3ea6007ab71526250543777c8046
Victoria <mvictoria.cavo@gmail.com>
2015-11-25 08:47:31 -0300

    file name

--- Ex1.rb
+++ Ex1.rb
@@ -13,6 +13,9 @@ end
 
 genbank_content = Bio::GenBank.open(file_name)
 
+file_split = file_name.split("/")
+file_name = file_split.last
+
 string_sequence = ''
 
 genbank_content.each_entry do |gc|
@@ -21,7 +24,7 @@ end
 
 6.times do |frame|
   Dir.mkdir("output") unless File.exists?("output")
-  File.open("output/fastaframe#{frame + 1}.fasta", 'w') do |f|
+  File.open("output/#{file_name}-frame#{frame + 1}.fasta", 'w') do |f|
     f.write(Bio::Sequence::NA.new(string_sequence).translate(frame + 1,1,'_').to_fasta)
   end
 end
